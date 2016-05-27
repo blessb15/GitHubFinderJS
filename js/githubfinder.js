161825e1de600f568745d67d4901045c66d52b4f
blessb15 <blakebless7@gmail.com>
2016-05-27 10:40:26 -0700

    show users name on page if recognized by api

--- js/githubfinder.js
+++ js/githubfinder.js
@@ -3,10 +3,19 @@ var apiKey = require('./../.env').apiKey;
 exports.Git = function(){
 };
 
-exports.Git.prototype.getRepos = function(user){
+exports.Git.prototype.getRepos = function(user, displayDataFunction){
   $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
-    var user = response;
-    console.log(user);
+    displayDataFunction(response.name);
+    console.log(response);
+  }).fail(function(error){
+    $('#showUser').text("Error: Not Found");
   });
-  return user;
+};
+
+exports.Git.prototype.getReposImg = function(user, displayDataFunction){
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+    displayDataFunction(response.avatar_url);
+  }).fail(function(error){
+    $('#showUser').text("Error: Not Found");
+  });
 };
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,11 +1,16 @@
 var Git = require('./../js/githubfinder.js').Git;
 
+var displayData = function(response) {
+  $('#showUser').text(response);
+};
+
 $(document).ready(function() {
-  var git = new Git();
+  var gitObject = new Git();
   $('#userFind').click(function() {
     var user = $('#user').val();
-    var newUser = git.getRepos(user);
-    console.log(newUser);
-    $("#showUser").text(newUser);
+    var userName = gitObject.getRepos(user, displayData);
+    var userImg = gitObject.getReposImg(user, displayData);
+    // $("#showImg").text("<img src='" + userImg + "'/>");
+    $("#showUser").text(userName);
   });
 });
