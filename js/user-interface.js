16e59f1bbd3846a1da5368e8dedb250238beb458
blessb15 <blakebless7@gmail.com>
2016-05-27 11:16:47 -0700

    show user profile picture above users name.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,16 +1,18 @@
 var Git = require('./../js/githubfinder.js').Git;
 
-var displayData = function(response) {
-  $('#showUser').text(response);
+var displayName = function(response) {
+  $('#showUser').html("<h3>" + response + "</h3>");
+};
+
+var displayImg = function(response) {
+  $('#showImg').html('<img src="' + response + '" width="150px" class="round"/>');
 };
 
 $(document).ready(function() {
   var gitObject = new Git();
   $('#userFind').click(function() {
     var user = $('#user').val();
-    var userName = gitObject.getRepos(user, displayData);
-    var userImg = gitObject.getReposImg(user, displayData);
-    // $("#showImg").text("<img src='" + userImg + "'/>");
-    $("#showUser").text(userName);
+    var userName = gitObject.getRepos(user, displayName);
+    var userImg = gitObject.getReposImg(user, displayImg);
   });
 });
