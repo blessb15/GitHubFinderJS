fe5cd8bedc32cc519d32d96fab32138ee2db5f7b
blessb15 <blakebless7@gmail.com>
2016-05-27 11:39:22 -0700

    add styling elements to the page.

--- js/githubfinder.js
+++ js/githubfinder.js
@@ -3,7 +3,7 @@ var apiKey = require('./../.env').apiKey;
 exports.Git = function(){
 };
 
-exports.Git.prototype.getRepos = function(user, displayDataFunction){
+exports.Git.prototype.getReposName = function(user, displayDataFunction){
   $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
     displayDataFunction(response.name);
     console.log(response);
@@ -16,6 +16,14 @@ exports.Git.prototype.getReposImg = function(user, displayDataFunction){
   $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
     displayDataFunction(response.avatar_url);
   }).fail(function(error){
-    $('#showUser').text("Error: Not Found");
+    $('#showImg').text("Error: Not Found");
+  });
+};
+
+exports.Git.prototype.getRepos = function(user, displayDataFunction){
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+    displayDataFunction(response.public_repos);
+  }).fail(function(error){
+    $('#showRepos').text("Error: Not Found");
   });
 };
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,18 +1,23 @@
 var Git = require('./../js/githubfinder.js').Git;
 
 var displayName = function(response) {
-  $('#showUser').html("<h3>" + response + "</h3>");
+  $('#showUser').html("<h3>" + response + "</h3><hr>");
 };
 
 var displayImg = function(response) {
   $('#showImg').html('<img src="' + response + '" width="150px" class="round"/>');
 };
 
+var displayRepos = function(response) {
+  $('#showRepos').html("Public Repos: " + response + "");
+};
+
 $(document).ready(function() {
   var gitObject = new Git();
   $('#userFind').click(function() {
     var user = $('#user').val();
-    var userName = gitObject.getRepos(user, displayName);
+    var userName = gitObject.getReposName(user, displayName);
     var userImg = gitObject.getReposImg(user, displayImg);
+    var userRepos = gitObject.getRepos(user, displayRepos);
   });
 });
